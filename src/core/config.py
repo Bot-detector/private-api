06ef3050d196100b65aee2c5cef40ef25c332649
RusticPotato <3542090+RusticPotatoes@users.noreply.github.com>
2023-10-16 16:03:29 -0400

    Stability updates (#2)
    * stability changes from public-api
    
    * makefile init
    
    * version 41.0.3
    
    * add verbose to docker commands
    
    * version changed to .4
    
    * minor fixes
    
    ---------
    
    Co-authored-by: extreme4all <40169115+extreme4all@users.noreply.github.com>

--- src/core/config.py
+++ src/core/config.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ENV: str
     DATABASE_URL: str
-    KAFKA_HOST: str = "localhost:9094"
+    KAFKA_HOST: str
+    POOL_RECYCLE: int
+    POOL_TIMEOUT: int
 
 settings = Settings()
